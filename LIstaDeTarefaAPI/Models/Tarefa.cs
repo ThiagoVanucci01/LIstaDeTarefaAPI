4c02ca0cb7ff6f6c7f1e60f42b144adbe22f2d59
ThiagoVanucci <thiago.dev24@gmail.com>
2025-02-04 15:27:21 -0300

    Inclusão dos pacotes NuGet no EntityFramework

---
+++ LIstaDeTarefaAPI/Models/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace LIstaDeTarefaAPI.Models
+{
+    public class Tarefa
+    { public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; }
+        public int TipoTarefaId { get; set; }
+        public TipoTarefa? TipoTarefa { get; set; }
+    }
+}
---
+++ LIstaDeTarefaAPI/Models/TipoTarefa.cs
@@ -0,0 +1,8 @@
+﻿namespace LIstaDeTarefaAPI.Models
+{
+    public class TipoTarefa
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+    }
+}
